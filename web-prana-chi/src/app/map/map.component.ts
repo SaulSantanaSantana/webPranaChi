bfc806e313e0a095f8dc781b415ab89de95e7141
saulsantanasantana <saulsantanasantana@gmail.com>
2024-05-28 12:10:18 +0100

    feat: updated map

--- web-prana-chi/src/app/map/map.component.ts
+++ web-prana-chi/src/app/map/map.component.ts
@@ -1,35 +1,58 @@
 import { Component, AfterViewInit } from '@angular/core';
-// @ts-ignore
-import * as L from 'leaflet';
+import { Geolocation } from '@capacitor/geolocation'
+import * as L from 'leaflet'
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
 @Component({
   selector: 'app-map',
-  standalone: true,
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.scss'],
 })
+export class MapComponent  implements AfterViewInit {
 
-export class MapComponent implements AfterViewInit {
-  private map: any;
+  private map:L.Map | undefined;
+  private markerLayer: L.LayerGroup| undefined
+  provider:OpenStreetMapProvider;
 
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
-    });
-
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    tiles.addTo(this.map);
+  constructor() { 
+    this.provider = new OpenStreetMapProvider();
   }
 
-  constructor() { }
 
-  ngAfterViewInit(): void {
-    this.initMap();
+  async ngAfterViewInit(): Promise<void> {
+    await this.initMap()    
   }
+
+  async initMap(){
+
+    let location
+
+    try {
+      await Geolocation.requestPermissions();
+      location = await Geolocation.getCurrentPosition({enableHighAccuracy:true})
+    } catch (error) {
+      location = await Geolocation.getCurrentPosition()
+    }
+
+    this.map = L.map('map',{
+      center:[location.coords.latitude,location.coords.longitude],
+      zoom: 18,
+      zoomControl: false
+    })
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+      maxZoom:20,
+      minZoom:3
+    })
+
+    tiles.addTo(this.map)
+
+    this.map.on('moveend', async () => {
+      const center = this.map?.getCenter();
+      if (center == undefined) {
+        return;
+      }
+    });
+  }
+
 }
