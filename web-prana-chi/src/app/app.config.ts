94ae347fae84ec1785f49f4e1393847531436f3c
saulsantanasantana <saulsantanasantana@gmail.com>
2024-06-03 12:03:25 +0100

    feat: added firestore

--- web-prana-chi/src/app/app.config.ts
+++ web-prana-chi/src/app/app.config.ts
@@ -3,7 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"pranachi","appId":"1:80307651290:web:73a4761bb85adc2bfc5be3","storageBucket":"pranachi.appspot.com","apiKey":"AIzaSyDs64TxAON0WgQej60tQPGx5UoQC9P6VB4","authDomain":"pranachi.firebaseapp.com","messagingSenderId":"80307651290"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
 };
---
+++ web-prana-chi/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyDs64TxAON0WgQej60tQPGx5UoQC9P6VB4",
+        authDomain: "pranachi.firebaseapp.com",
+        projectId: "pranachi",
+        storageBucket: "pranachi.appspot.com",
+        messagingSenderId: "80307651290",
+    appId: "1:80307651290:web:73a4761bb85adc2bfc5be3"
+    }
+};
