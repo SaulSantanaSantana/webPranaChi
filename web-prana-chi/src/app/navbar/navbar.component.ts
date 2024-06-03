dbc8d09d3118b6aca3d62cdb366c68f8405915fe
saulsantanasantana <saulsantanasantana@gmail.com>
2024-06-03 12:32:13 +0100

    feat: added log and register component

--- web-prana-chi/src/app/navbar/navbar.component.ts
+++ web-prana-chi/src/app/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LogInComponent } from '../log-in/log-in.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [LogInComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
