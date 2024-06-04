559d7fde363422005f1c03ac7e7ed4dd0e2822a5
saulsantanasantana <saulsantanasantana@gmail.com>
2024-06-04 11:01:17 +0100

    feat: added style to account forms

--- web-prana-chi/src/app/log-in/log-in.component.ts
+++ web-prana-chi/src/app/log-in/log-in.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [RegisterComponent],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
 })
--- web-prana-chi/src/app/navbar/navbar.component.ts
+++ web-prana-chi/src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LogInComponent } from '../log-in/log-in.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [LogInComponent],
+  imports: [LogInComponent, RegisterComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
