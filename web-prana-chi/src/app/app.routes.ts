3fddf435e561bb399d990b7ef6f4f69a0f6228e0
saulsantanasantana <saulsantanasantana@gmail.com>
2024-06-17 13:31:21 +0100

    feat: added style to activities

--- web-prana-chi/src/app/app.routes.ts
+++ web-prana-chi/src/app/app.routes.ts
@@ -6,13 +6,14 @@ import { ProfileComponent } from './profile/profile.component';
 import { DocumentosComponent } from './documentos/documentos.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { PagosComponent } from './pagos/pagos.component';
+import { ActivitiesComponent } from './activities/activities.component';
 
 export const routes: Routes = [
     {path: 'actividades', component: ActividadesComponent},
     {path: 'conocenos', component: ConocenosComponent},
     {path: 'pagos', component: PagosComponent},
     {path: 'perfil', component: ProfileComponent},
-    {path: 'actividades', component: ActividadesComponent},
+    {path: 'actividades-usuario', component: ActivitiesComponent},
     {path: 'documentos', component: DocumentosComponent},
     {path: 'notificaciones', component: NotificationsComponent},
     {path: '**', component: LandingComponent}
