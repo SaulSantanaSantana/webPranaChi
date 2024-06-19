ec25d418de144a96fdf6aea6a8aa590a95182a01
saulsantanasantana <saulsantanasantana@gmail.com>
2024-06-19 12:39:14 +0100

    Feat: finla style for notifications

---
+++ web-prana-chi/src/app/notification-modal/notification-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './notification-modal.component.html',
+  styleUrl: './notification-modal.component.css'
+})
+export class NotificationModalComponent {
+
+}
--- web-prana-chi/src/app/notifications/notifications.component.ts
+++ web-prana-chi/src/app/notifications/notifications.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NotificationModalComponent } from '../notification-modal/notification-modal.component';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [],
+  imports: [NotificationModalComponent],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.css'
 })
