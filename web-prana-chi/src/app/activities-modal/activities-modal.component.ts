f002d0b1703ff3c3866677166446b698020e2c93
saulsantanasantana <saulsantanasantana@gmail.com>
2024-06-19 14:15:29 +0100

    feat: finished modal styles

---
+++ web-prana-chi/src/app/activities-modal/activities-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activities-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './activities-modal.component.html',
+  styleUrl: './activities-modal.component.css'
+})
+export class ActivitiesModalComponent {
+
+}
--- web-prana-chi/src/app/activities/activities.component.ts
+++ web-prana-chi/src/app/activities/activities.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ActivitiesModalComponent } from '../activities-modal/activities-modal.component';
 
 @Component({
   selector: 'app-activities',
   standalone: true,
-  imports: [],
+  imports: [ActivitiesModalComponent],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.css'
 })
