81e7899e8d56a0eeab13b2037e307b0617ba6038
saulsantanasantana <saulsantanasantana@gmail.com>
2024-07-02 11:06:43 +0100

    feat: Sorted general acces

--- web-prana-chi/src/app/app.component.ts
+++ web-prana-chi/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LandingComponent } from './landing/landing.component';
+import { LandingComponent } from './General/landing/landing.component';
 import { FooterComponent } from './footer/footer.component';
-import { ActividadesComponent } from './actividades/actividades.component';
+import { ActividadesComponent } from './General/actividades/actividades.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
--- web-prana-chi/src/app/app.routes.ts
+++ web-prana-chi/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import {Routes} from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
-import { ActividadesComponent } from './actividades/actividades.component';
-import { ConocenosComponent } from './conocenos/conocenos.component';
+import { LandingComponent } from './General/landing/landing.component';
+import { ActividadesComponent } from './General/actividades/actividades.component';
+import { ConocenosComponent } from './General/conocenos/conocenos.component';
 import { ProfileComponent } from './profile/profile.component';
 import { DocumentosComponent } from './documentos/documentos.component';
 import { NotificationsComponent } from './notifications/notifications.component';
