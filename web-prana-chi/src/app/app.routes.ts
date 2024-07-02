9e21bd152eb775fce024b41f470235744483448e
saulsantanasantana <saulsantanasantana@gmail.com>
2024-07-02 11:42:11 +0100

    fix: added guards to navigation

--- web-prana-chi/src/app/app.routes.ts
+++ web-prana-chi/src/app/app.routes.ts
@@ -7,14 +7,15 @@ import { DocumentosComponent } from './Users/documentos/documentos.component';
 import { NotificationsComponent } from './Users/notifications/notifications.component';
 import { PagosComponent } from './Users/pagos/pagos.component';
 import { ActivitiesComponent } from './Users/activities/activities.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: 'oferta-actividades', component: ActividadesComponent},
     {path: 'conocenos', component: ConocenosComponent},
-    {path: 'pagos', component: PagosComponent},
-    {path: 'perfil', component: ProfileComponent},
-    {path: 'actividades', component: ActivitiesComponent},
-    {path: 'documentos', component: DocumentosComponent},
-    {path: 'notificaciones', component: NotificationsComponent},
+    {path: 'pagos', component: PagosComponent, canActivate: [authGuard]},
+    {path: 'perfil', component: ProfileComponent, canActivate: [authGuard]},
+    {path: 'actividades', component: ActivitiesComponent, canActivate: [authGuard]},
+    {path: 'documentos', component: DocumentosComponent, canActivate: [authGuard]},
+    {path: 'notificaciones', component: NotificationsComponent, canActivate: [authGuard]},
     {path: '**', component: LandingComponent}
 ];
